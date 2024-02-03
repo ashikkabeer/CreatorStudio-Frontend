1a9c7f451e2f8c304dde032dfa06799619681ea8
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-02-03 23:05:35 +0530

    hero page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,14 @@ import { ModeToggle } from './toggle-theme';
 
 export default function Navbar() {
   return (
-    <div className='flex w-screen pt-3 items-center'>
-      <div className='flex justify-center w-full items-center gap-4 md:justify-center'>
+    <div className='flex w-screen pt-3 items-center fixed top-0'>
+      <div className='flex justify-center w-full items-center  md:gap-9 gap-4 md:justify-center'>
         <div>Home</div>
         <div>Dashboard</div>
         <div>Home</div>
         <div className='flex justify-end'></div>
       </div>
-      <div className='pr-2'>
+      <div className='pr-2 md:pr-7'>
         <ModeToggle />
       </div>
     </div>
