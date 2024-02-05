9286856d1a04e17a6f6759e108c9f704b6a070aa
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-02-05 22:04:57 +0530

    removed the name in the nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,16 +3,10 @@ import { ModeToggle } from './toggle-theme';
 export default function Navbar() {
   return (
     <div className=' flex w-screen pt-3 items-center top-0'>
-      <div className='pl-5 m-0'>
-        <p className=''>
-          Creator<span className='text-primary'>Studio</span>
-        </p>
-      </div>
       <div className='flex font-semibold justify-center w-full items-center  md:gap-9 gap-4 md:justify-center'>
         <div>Home</div>
         <div>Dashboard</div>
         <div>Account</div>
-        <div>Settings</div>
         <div className='flex justify-end'></div>
       </div>
       <div className='pr-2 md:pr-7'>
